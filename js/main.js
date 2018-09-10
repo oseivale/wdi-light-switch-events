43b7b5c3af02bdfced990632390f15098561c29d
Valerie Osei <valerie.osei@gmail.com>
2018-09-10 17:01:07 -0400

    used event listeners to manipulate the light switch and background color of the body

--- js/main.js
+++ js/main.js
@@ -1,2 +1,24 @@
+document.addEventListener('DOMContentLoaded', function(){
+  var switchOff = document.querySelector('.switch');
+  var shade = document.querySelector('.status')
+  var dark = document.querySelector('body')
 
 
+   switchOff.addEventListener('click',function(){
+     switchOff.classList.toggle('on');
+     switchOff.classList.toggle('off');
+     dark.classList.toggle('dark');
+     dark.classList.toggle('light');
+
+
+     if (switchOff.classList.contains('off')){
+             shade.innerHTML = "Hey, who turned off the lights?";
+      } else {
+
+        shade.innerHTML = "It's so bright in here!";
+      }
+
+
+ })
+
+});
